bdd6418a8dc6fef1025ea9878f73345fec4619c2
emu <himtiaz@dohatec.com.bd>
2017-02-16 13:44:23 +0600

    uploaders added for models

--- src/directors/models.py
+++ src/directors/models.py
@@ -5,9 +5,17 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 
+def upload_location(instance, filename):
+    return "%s/%s/%s" % (instance.__class__.__name__, instance.id, filename)
+
 class Director(models.Model):
     name = models.CharField(max_length=120)
     bio = models.TextField()
+    photo = models.ImageField(upload_to=upload_location, blank=True, null=True,
+                              width_field="width_field",
+                              height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
--- src/movies/models.py
+++ src/movies/models.py
@@ -3,19 +3,29 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+def upload_location(instance, filename):
+    return "%s/%s/%s" % (instance.__class__.__name__, instance.id, filename)
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=120)
     plot = models.TextField()
     release_date = models.DateTimeField()
     rating = models.FloatField(default=0.0)
+    photo = models.ImageField(upload_to=upload_location, blank=True, null=True,
+                              width_field="width_field",
+                              height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     directors = models.ManyToManyField('directors.Director')
     actors = models.ManyToManyField('actors.Actor')
     user_rating = models.ManyToManyField(User, through='MovieRating')
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
-
 
     def __unicode__(self):
         return unicode(self.name)
